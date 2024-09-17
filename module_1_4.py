64acad666e74173107524e1e90f255015dd57d50
ElizavetaZeli <kelli.ellison@mail.ru>
2024-09-17 19:40:14 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите фразу которой придерживайтесь по жизни: ")
+print("Колличество символов в введенном тексте: ", len(my_string))
+print("Строка в верхнем регистре: ", my_string.upper())
+print("Строка в нижнем регистре: ", my_string.lower())
+print(my_string.replace(" ",""))
+print("Первый символ строки: ", my_string[0])
+print("Последний символ строки:", my_string[-1])
